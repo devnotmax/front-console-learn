d964cbd9df82183bd5134655fc93f34bc6babf9e
Monacada16 <juanxomoncada@gmail.com>
2024-10-16 18:23:15 -0500

    creacion del login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "@/components/Login/login";
+
+const login = () => {
+    return (
+        <section>
+            <Login/>
+        </section>
+    );
+};
+
+export default login;
