3f5f932c7d00abbc9e2ef90be38f137c60ba9f42
Monacada16 <juanxomoncada@gmail.com>
2024-10-15 19:28:44 -0500

    se agrega componente de review con mocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "@/components/Hero/Hero";
+import UserReview from "@/components/UserReview/UserReviews";
 
 export default function Home() {
   return (
     <main className="container">
       <Hero></Hero>
+      <UserReview />
     </main>
   );
 }
