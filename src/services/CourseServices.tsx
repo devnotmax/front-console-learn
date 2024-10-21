741720a4f2189202b7ec7bf9357f5adb7192cc2b
devnotmax <develop.maxsj@gmail.com>
2024-10-21 20:38:45 -0300

    filtros agregados

---
+++ src/services/CourseServices.tsx
@@ -0,0 +1,13 @@
+import { ICourse } from "@/interfaces/Course";
+
+export const getCourses = async (): Promise<ICourse[]> => {
+    try {
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+        const res = await fetch(`${apiUrl}/courses`);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error("Error al obtener los datos de los cursos:", error);
+        return [];
+    }
+}
