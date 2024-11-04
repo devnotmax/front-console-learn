fce4f83b770d8c0d54860e8d1430a72dbf82a05d
devnotmax <develop.maxsj@gmail.com>
2024-11-04 12:09:35 -0300

    Arregle el course details y ejecute el build correctamente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "@/components/Hero/Hero";
 import Carrousel from "@/components/Carrousel/Carrousel";
-import ReviewCourse from "@/components/UserReview/ReviewCourse";
 import ReviewsContainer from "@/components/UserReview/ReviewsContainer";
 
 export default function Home() {
