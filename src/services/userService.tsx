e00c51e497d07d5e08add70e4e98e857c5118e01
devnotmax <develop.maxsj@gmail.com>
2024-11-05 12:36:44 -0300

    admin panel listo

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -1,25 +1,47 @@
+import { getAuthToken } from "@/contexts/authContext";
+
 export const uploadProfileImage = async (file: File) => {
-    const formData = new FormData();
-    formData.append("profileImage", file);
+  const formData = new FormData();
+  formData.append("profileImage", file);
 
-    try {
-        const apiURL = process.env.NEXT_PUBLIC_API_URL;
-        const response = await fetch(`${apiURL}/users/upload`, {
-            method: "POST",
-            body: formData,
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-        })
+  try {
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+    const response = await fetch(`${apiURL}/users/upload`, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error("Error uploading profile image");
-        }
-
-        const data = await response.json();
-        return data.profileImageUrl;
-    } catch (error) {
-        console.error("Error uploading profile image:", error);
-        throw error;
+    if (!response.ok) {
+      throw new Error("Error uploading profile image");
     }
+
+    const data = await response.json();
+    return data.profileImageUrl;
+  } catch (error) {
+    console.error("Error uploading profile image:", error);
+    throw error;
+  }
+};
+
+export const getUsers = async () => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const token = getAuthToken();
+
+  const res = await fetch(`${apiUrl}/users`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Error fetching users");
+  }
+
+  const data = await res.json();
+  return data;
 };
