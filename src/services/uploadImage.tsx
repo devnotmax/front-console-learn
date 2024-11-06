8d3cf3dcf121d5ff987532408505d6031670b1cf
devnotmax <develop.maxsj@gmail.com>
2024-11-05 21:17:51 -0300

    Profile listo y funcionando con estados globales

--- src/services/uploadImage.tsx
+++ src/services/uploadImage.tsx
@@ -1,7 +1,28 @@
 import { getAuthToken } from "@/contexts/authContext";
 
 // services/uploadImage.ts
-export async function uploadProfileImage(image: File): Promise<Response> {
+// export async function uploadProfileImage(image: File): Promise<Response> {
+//     const token = getAuthToken();
+//     const apiURL = process.env.NEXT_PUBLIC_API_URL;
+//     const formData = new FormData();
+//     formData.append('image', image);
+
+//     const response = await fetch(`${apiURL}/files/user`, {
+//         method: "POST",
+//         headers: {
+//             // Asegúrate de agregar el token si es necesario
+//             "Authorization": `Bearer ${token}`,
+//         },
+//         body: formData,
+//     });
+
+//     if (!response.ok) {
+//         throw new Error("Error uploading image");
+//     }
+
+//     return response;
+// }
+export async function uploadProfileImage(image: File): Promise<string> {
     const token = getAuthToken();
     const apiURL = process.env.NEXT_PUBLIC_API_URL;
     const formData = new FormData();
@@ -10,7 +31,6 @@ export async function uploadProfileImage(image: File): Promise<Response> {
     const response = await fetch(`${apiURL}/files/user`, {
         method: "POST",
         headers: {
-            // Asegúrate de agregar el token si es necesario
             "Authorization": `Bearer ${token}`,
         },
         body: formData,
@@ -18,7 +38,12 @@ export async function uploadProfileImage(image: File): Promise<Response> {
 
     if (!response.ok) {
         throw new Error("Error uploading image");
+        console.log("Error uploading image:", response.status);
     }
 
-    return response;
+    // Procesar la respuesta para obtener la URL de la imagen
+    const responseData = await response.json();
+    return responseData.url; // Cambia 'url' por la clave correcta en tu respuesta JSON
+
+    console.log("URL de la imagen:", responseData.url);
 }
